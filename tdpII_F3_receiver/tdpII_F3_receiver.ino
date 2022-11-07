dda5846cfa1e7837248f9ffae64f7ae7598637d4
Tomás Dozo <tomasdozo@hotmail.com>
2022-11-06 23:49:15 -0300

    Codigo base

--- tdpII_F3_receiver/tdpII_F3_receiver.ino
+++ tdpII_F3_receiver/tdpII_F3_receiver.ino
@@ -9,38 +9,39 @@ RF24 radio(9, 8);  // CE, CSN
 //address through which two modules communicate. Any 5-character string
 const byte address[6] = "tomas";
 
-void setup()
-{
+void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ;  // wait for serial port to connect. Needed for native USB port only
   }
 
   // testing Serial monitor with a welcome message
   Serial.println("Hello World!");
 
   // Initialize device.
-  radio.begin();
-  
+  if (!radio.begin()) {
+    Serial.println("radio hardware is not responding!!");
+    while (1) {
+    }
+  }
+
   // Set the address. The first argument is the number of the stream, we only created one
   radio.openReadingPipe(0, address);
-  
+
   // Set module as receiver
   radio.startListening();
 }
 
-void loop()
-{
+void loop() {
   //Read the data if available in buffer
-  if (radio.available())
-  {
-    // Create an array of 32 characters filled with zeros 
-    char text[32] = {0};
+  if (radio.available()) {
+    // Create an array of 32 characters filled with zeros
+    char text[32] = { 0 };
     // Read data from the buffer and store it in the character array
     radio.read(&text, sizeof(text));
     // Print data on the Serial monitor
     Serial.println(text);
-    Serial.println("Bye World!");
+    Serial.println("----------");
   }
 }
