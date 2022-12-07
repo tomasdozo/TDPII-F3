a7ee4604a4eca3d21439c748d5c10ad652ea7067
Lourdes Valli <lourvalli@gmail.com>
2022-12-06 21:18:19 -0300

    usage of another simpler library for reading data from dht sensor

--- NRF Transmitter/NRF Transmitter.ino
+++ NRF Transmitter/NRF Transmitter.ino
@@ -1,31 +1,10 @@
-// DHT Temperature & Humidity Sensor
-// Unified Sensor Library Example
-// Written by Tony DiCola for Adafruit Industries
-// Released under an MIT license.
-
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
-
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-#include <SPI.h>
+#include "dht11.h"
 #include "RF24.h"
 
-#define DHTPIN 2  // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
+dht11 DHT11;
 
-// Uncomment the type of sensor in use:
-#define DHTTYPE    DHT11     // DHT 11
-//#define DHTTYPE    DHT22  // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+#define DHT11_PIN 2
 
-// See guide for details on sensor wiring and usage:
-//   https://learn.adafruit.com/dht/overview
-
-DHT_Unified dht(DHTPIN, DHTTYPE);
 //create an RF24 object
 RF24 radio(9, 8);  // CE, CSN
 const byte address[6] = "tomas";
@@ -34,11 +13,9 @@ char temperature[4];
 char humidity [2]="10";
 unsigned int counter = 1;
 
-
-uint32_t delayMS;
-
 void setup() {
   Serial.begin(9600);
+  Serial.println("DHT11 SFE Electronics");
 
   // Initialize device.
   if (!radio.begin()) {
@@ -50,81 +27,31 @@ void setup() {
   //set the address
   radio.openWritingPipe(address);
 
-  dht.begin();
-  Serial.println(F("DHT11 Unified Sensor"));
-
-  // Print temperature sensor details.
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("C"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("C"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("C"));
-  Serial.println(F("------------------------------------"));
-
-  // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("%"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("%"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 500;
+  int chk = DHT11.read(DHT11_PIN);
+  Serial.println(chk);
 }
 
 void loop() {
   // Delay between measurements.
-  delay(delayMS);
-  // Get temperature event and print its value.
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
+  delay(1000);
+
+  int chk = DHT11.read(DHT11_PIN);
+  
+  if (chk) {
+    Serial.println(F("Error reading sensor!"));
   } else {
     Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
+    Serial.print((float)DHT11.temperature, 1);
     Serial.println(F("C"));
-    dtostrf(event.temperature,4,1,temperature);
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  } else {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
-    dtostrf(event.relative_humidity,2,0,humidity);
+    dtostrf(DHT11.temperature,4,1,temperature);
 
+    Serial.print(F("Humidity: "));
+    Serial.print((float)DHT11.humidity, 1);
+    Serial.println(F("%"));
+    dtostrf(DHT11.humidity,2,0,humidity);
   }
-  sprintf(text,"Temp: %s C. Hum: %s%%, n: %i",temperature,humidity, counter);
+
+  sprintf(text,"Temp: %s C. Hum: %s%%, n: %i",temperature, humidity, counter);
   if(radio.write(&text, sizeof(text))){
     // Mensaje recibido
     Serial.print("SUCCESS! n: ");
